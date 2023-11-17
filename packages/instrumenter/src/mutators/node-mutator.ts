db0de58569bcd8e1b251d17729d245eae4ec4d3e
Javier <j.perezyperez@student.utwente.nl>
2023-11-17 10:37:17 +0100

    Adaptation of the NodeMutator

--- packages/instrumenter/src/mutators/node-mutator.ts
+++ packages/instrumenter/src/mutators/node-mutator.ts
@@ -1,6 +1,6 @@
 import type { types, NodePath } from '@babel/core';
 
 export interface NodeMutator {
-  mutate(path: NodePath): Iterable<types.Node>;
+  mutate(path: NodePath, operations: String[] | undefined): Iterable<types.Node>;
   readonly name: string;
 }
