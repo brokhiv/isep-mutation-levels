2a0c326ffb5ac220bab0bf35fb0aaa4c2a20663d
Luc Timmerman <timmerman.luc1999@gmail.com>
2023-11-17 09:13:58 +0100

    Split *mutate into two functions

--- packages/instrumenter/src/mutators/boolean-literal-mutator.ts
+++ packages/instrumenter/src/mutators/boolean-literal-mutator.ts
@@ -12,30 +12,39 @@ export const booleanLiteralMutator: NodeMutator = {
   name: 'BooleanLiteral',
 
   *mutate(path, level: MutationLevel | undefined) {
-    if (level === undefined) {
-      // No mutation level selected, allow everything.
+    if (isInMutationLevel(path, level)) {
       if (path.isBooleanLiteral()) {
         yield types.booleanLiteral(!path.node.value);
       }
       if (path.isUnaryExpression() && path.node.operator === '!' && path.node.prefix) {
         yield deepCloneNode(path.node.argument);
       }
-    } else if (level.BooleanLiteral !== undefined) {
-      // Mutation level has been set and the included BooleanLiterals are defined
-      if (path.isBooleanLiteral()) {
-        const value: boolean = path.node.value;
-        if (
-          (value && level.BooleanLiteral.some((lit) => lit == 'TrueToFalse')) ||
-          (!value && level.BooleanLiteral.some((lit) => lit == 'FalseToTrue'))
-        ) {
-          yield types.booleanLiteral(!path.node.value);
-        }
-      }
-      if (path.isUnaryExpression() && path.node.operator === '!' && path.node.prefix) {
-        if (level.BooleanLiteral.some((lit) => lit == 'RemoveNegation')) {
-          yield deepCloneNode(path.node.argument);
-        }
-      }
     }
   },
 };
+
+function isInMutationLevel(node: any, level?: MutationLevel): boolean {
+  // No mutation level selected, allow everything.
+  if (level === undefined) {
+    return true;
+  }
+  // Mutation level selected, BooleanLiteral not included, block everything.
+  if (level.BooleanLiteral === undefined) {
+    return false;
+  }
+  if (path.isBooleanLiteral()) {
+    // Path is a boolean literal. If the value is true, check if TrueToFalse is included. Otherwise, check if
+    //  FalseToTrue is included.
+    const value: boolean = path.node.value;
+    if (
+      (value && level.BooleanLiteral.some((lit) => lit == 'TrueToFalse')) ||
+      (level.BooleanLiteral.some((lit) => lit == 'FalseToTrue'))
+    ) {
+      return true;
+    }
+  }
+  // Path is a unary expression, which would be negation (!) in our case. Check if RemoveNegation is included.
+  if (path.isUnaryExpression() && path.node.operator === '!' && path.node.prefix && level.BooleanLiteral.some((lit) => lit == 'RemoveNegation')) {
+    return true;
+  }
+}
