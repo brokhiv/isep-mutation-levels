6d7621ca1bebc2126caa43e03cc824cde194d921
Javier <j.perezyperez@student.utwente.nl>
2023-11-17 10:37:47 +0100

    Added check to only run desired suboperators

--- packages/instrumenter/src/mutators/array-declaration-mutator.ts
+++ packages/instrumenter/src/mutators/array-declaration-mutator.ts
@@ -1,20 +1,23 @@
 import babel, { type NodePath } from '@babel/core';
-
 import { deepCloneNode } from '../util/index.js';
-
 import { NodeMutator } from './node-mutator.js';
 
 const { types } = babel;
 
+const arrayDeclarationReplacements = [
+'EmptyArray',
+'EmptyConstructor' ,
+] as const ;
+
 export const arrayDeclarationMutator: NodeMutator = {
   name: 'ArrayDeclaration',
 
-  *mutate(path: NodePath): Iterable<babel.types.Node> {
-    if (path.isArrayExpression()) {
+  *mutate(path: NodePath, operations: String[] | undefined): Iterable<babel.types.Node> {
+   if (path.isArrayExpression() && (operations == undefined || operations.includes(arrayDeclarationReplacements[0]))) {
       const replacement = path.node.elements.length ? types.arrayExpression() : types.arrayExpression([types.stringLiteral('Stryker was here')]);
       yield replacement;
     }
-    if ((path.isCallExpression() || path.isNewExpression()) && types.isIdentifier(path.node.callee) && path.node.callee.name === 'Array') {
+    if ((path.isCallExpression() || path.isNewExpression()) && types.isIdentifier(path.node.callee) && path.node.callee.name === 'Array' && ( operations == undefined || operations.includes(arrayDeclarationReplacements[1] ))) {
       const mutatedCallArgs = path.node.arguments.length ? [] : [types.arrayExpression()];
       const replacement = types.isNewExpression(path.node)
         ? types.newExpression(deepCloneNode(path.node.callee), mutatedCallArgs)
@@ -22,4 +25,4 @@ export const arrayDeclarationMutator: NodeMutator = {
       yield replacement;
     }
   },
-};
+}
