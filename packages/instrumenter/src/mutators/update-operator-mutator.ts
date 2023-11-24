57cca18e4342b1c3bc570541e4629fc27be05123
Javier <j.perezyperez@student.utwente.nl>
2023-11-24 17:46:14 +0100

    updated to conform to mutationlevels

--- packages/instrumenter/src/mutators/update-operator-mutator.ts
+++ packages/instrumenter/src/mutators/update-operator-mutator.ts
@@ -6,17 +6,39 @@ import { NodeMutator } from './index.js';
 
 const { types } = babel;
 
-enum UpdateOperators {
-  '++' = '--',
-  '--' = '++',
-}
+const UpdateReplacements = Object.assign({
+  'Post++To--': { replacementOperator: '--', mutatorName: 'Post++To--' },
+  'Post--To++': { replacementOperator: '++', mutatorName: 'Post--To++' },
+  'Pre++To--': { replacementOperator: '--', mutatorName: 'Pre++To--' },
+  'Pre--To++': { replacementOperator: '++', mutatorName: 'Pre--To++' },
+  '++': { replacementOperator: '--', mutatorName: '++all' },
+  '--': { replacementOperator: '++', mutatorName: '--all' },
+} as const);
 
 export const updateOperatorMutator: NodeMutator = {
   name: 'UpdateOperator',
 
-  *mutate(path) {
-    if (path.isUpdateExpression()) {
-      yield types.updateExpression(UpdateOperators[path.node.operator], deepCloneNode(path.node.argument), path.node.prefix);
+  *mutate(path, operations) {
+    if (path.isUpdateExpression() && operations === undefined) {
+      yield types.updateExpression(
+        UpdateReplacements[path.node.operator as keyof string].replacementOperator,
+        deepCloneNode(path.node.argument),
+        path.node.prefix,
+      );
+    } else if (path.isUpdateExpression() && operations !== undefined) {
+      let replacement = undefined;
+      if (path.node.prefix && path.node.operator == '++' && operations.includes(UpdateReplacements['Pre++To--'].mutatorName as string)) {
+        replacement = UpdateReplacements['Pre++To--'].replacementOperator;
+      } else if (path.node.prefix && path.node.operator == '--' && operations.includes(UpdateReplacements['Pre--To++'].mutatorName as string)) {
+        replacement = UpdateReplacements['Pre--To++'].replacementOperator;
+      } else if (!path.node.prefix && path.node.operator == '++' && operations.includes(UpdateReplacements['Post++To--'].mutatorName as string)) {
+        replacement = UpdateReplacements['Post++To--'].replacementOperator;
+      } else if (!path.node.prefix && path.node.operator == '--' && operations.includes(UpdateReplacements['Post--To++'].mutatorName as string)) {
+        replacement = UpdateReplacements['Post--To++'].replacementOperator;
+      }
+      if (replacement !== undefined) {
+        yield types.updateExpression(replacement, deepCloneNode(path.node.argument), path.node.prefix);
+      }
     }
   },
 };
