cdb647b6fa787cc8fb3f67726545c3c1d3a06a55
Javier <j.perezyperez@student.utwente.nl>
2024-01-09 13:53:34 +0100

    Made the boolean literal more consistent

--- packages/instrumenter/test/unit/mutators/boolean-literal-mutator.spec.ts
+++ packages/instrumenter/test/unit/mutators/boolean-literal-mutator.spec.ts
@@ -9,14 +9,12 @@ const booleanLiteralLevel: MutationLevel = {
   BooleanLiteral: ['TrueLiteralNegation', 'LogicalNotRemoval'],
 };
 
-const booleanLiteralAllLevel: MutationLevel = {
-  name: 'BooleanLiteralLevel',
-  BooleanLiteral: ['TrueLiteralNegation', 'FalseLiteralNegation', 'LogicalNotRemoval'],
-};
-
 const booleanLiteralUndefinedLevel: MutationLevel = {
   name: 'BooleanLiteralLevel',
+  BooleanLiteral: [],
 };
+
+const noLevel = undefined;
 
 describe(sut.name, () => {
   it('should have name "BooleanLiteral"', () => {
@@ -46,24 +44,13 @@ describe(sut.name, () => {
   });
 
   it('should not mutate anything if there are no values in the mutation level', () => {
-    expectJSMutationWithLevel(sut, [], 'if (true) {}; if (false) {}; if (!value) {}');
-  });
-
-  it('should mutate everything if everything is in the mutation level', () => {
-    expectJSMutationWithLevel(
-      sut,
-      booleanLiteralAllLevel.BooleanLiteral,
-      'if (true) {}; if (false) {}; if (!value) {}',
-      'if (false) {}; if (false) {}; if (!value) {}',
-      'if (true) {}; if (false) {}; if (value) {}',
-      'if (true) {}; if (true) {}; if (!value) {}',
-    );
+    expectJSMutationWithLevel(sut, booleanLiteralUndefinedLevel.BooleanLiteral, 'if (true) {}; if (false) {}; if (!value) {}');
   });
 
   it('should mutate everything if the mutation level is undefined', () => {
     expectJSMutationWithLevel(
       sut,
-      booleanLiteralUndefinedLevel.BooleanLiteral,
+      noLevel,
       'if (true) {}; if (false) {}; if (!value) {}',
       'if (false) {}; if (false) {}; if (!value) {}',
       'if (true) {}; if (false) {}; if (value) {}',
